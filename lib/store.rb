a930e313715b6cf6466c0411bbe60db60bb109c7
Nikko Bansil <bjohnnikko@gmail.com>
2023-01-23 21:40:56 -0800

    create a custom validation

--- lib/store.rb
+++ lib/store.rb
@@ -3,4 +3,11 @@ class Store < ActiveRecord::Base
 
   validates :name, length: {minimum: 3}
   validates :annual_revenue, numericality: { greater_than_or_equal_to: 0}
+  validate :men_or_womens_apparel
+
+  def men_or_womens_apparel
+    if mens_apparel.present? == false && womens_apparel.present? == false
+      errors.add(:need_an_apparel, "Stores must carry at least one of the men's or women's apparel")
+    end
+  end
 end
