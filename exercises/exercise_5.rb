21dd7f8a84be5c3846f8c8bbcef4fcbf7d759131
Nikko Bansil <bjohnnikko@gmail.com>
2023-01-23 20:57:11 -0800

    calculate total/average revenue and high performing stores

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -7,4 +7,12 @@ require_relative './exercise_4'
 puts "Exercise 5"
 puts "----------"
 
-# Your code goes here ...
+### Exercise 5: Calculations
+@total_revenue = Store.sum(:annual_revenue)
+puts @total_revenue
+
+@average_annual_revenue = Store.average(:annual_revenue)
+puts @average_annual_revenue
+
+@high_performing = Store.where(annual_revenue: 1000000...)
+puts @high_performing.count
